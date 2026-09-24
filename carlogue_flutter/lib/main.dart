8c853ae18e236a79d1817b6319355bb7bd2220cd
ferman557 <Ferman557@gmail.com>
2026-09-25 01:56:19 +0400

    Update main.dart

--- carlogue_flutter/lib/main.dart
+++ carlogue_flutter/lib/main.dart
@@ -5,6 +5,7 @@ import 'screens/greetings_screen.dart';
 
 void main() async {
   final greeting = await client.greeting.hello('World');
+  print(greeting);
   WidgetsFlutterBinding.ensureInitialized();
   await initializeClient();
   runApp(const MyApp());
